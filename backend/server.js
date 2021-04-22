c951a1f44478af2d5e9d6618f2857e18e47b7cae
ImNM <water0641@naver.com>
2021-04-22 20:43:17 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const db = require('./db');
+const app = express();
+
+app.use(bodyParser.json());
+
+// db.pool.query(`CREATE TABLE list(
+//     id INTEGER AUTO INCREMENT,
+//     value TEXT,
+//     PRIMARY KEY(id)
+// )`,(err,results,fileds) =>{
+//     console.log('results',results)
+// })
+
+app.get('/api/values',function(req,res){
+    db.pool.query('SELECT *FROM lists',
+    (err,results,fileds)=>{
+        if(err)
+            return res.status(500).send(err);
+        else
+            return res.json(results);
+
+    })
+})
+
+app.post('/api/value',function(req,res,next){
+    console.log(req.body.value)
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
+    (err,results,fileds)=>{
+        console.log(err)
+        if(err)
+            return res.status(500).send(err);
+        else
+            return res.json({success:true, value: req.body.value});
+
+    })
+})
+
+app.listen(5000,()=>{
+    console.log("실행 완료~");
+})
